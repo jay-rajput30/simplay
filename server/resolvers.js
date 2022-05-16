e441f4f69785523ec5bddb9ae006947c1bf507c8
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2022-05-16 06:51:51 +0530

    feat: added video model's  add/delete/find mutation and typedefs

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,43 @@
+const Video = require("./models/video.model");
+
 const resolvers = {
   Query: {
     hello: () => {
       return "hello world";
+    },
+    getAllVideos: async () => {
+      try {
+        return await Video.find();
+      } catch (e) {
+        console.error({ error: e });
+      }
+    },
+  },
+  Mutation: {
+    addVideo: async (parent, args, context, info) => {
+      try {
+        const { title, views, likes, link, category } = args.video;
+        const newVideo = new Video({ title, views, likes, link, category });
+        await newVideo.save();
+      } catch (e) {
+        console.error({ error: e });
+      }
+    },
+    getVideo: async (parent, args, context, info) => {
+      try {
+        const { id } = args;
+        return await Video.findById(id);
+      } catch (e) {
+        console.error({ error: e });
+      }
+    },
+    deleteVideo: async (parent, args, context, info) => {
+      try {
+        const { id } = args;
+        return await Video.findByIdAndDelete(id);
+      } catch (e) {
+        console.error({ error: e });
+      }
     },
   },
 };
--- server/typeDefs.js
+++ server/typeDefs.js
@@ -1,8 +1,31 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type Video {
+    id: ID!
+    title: String!
+    views: Int!
+    likes: Int!
+    link: String!
+    category: String!
+  }
   type Query {
     hello: String
+
+    getAllVideos: [Video]
+  }
+
+  input VideoInput {
+    title: String!
+    views: Int!
+    likes: Int!
+    link: String!
+    category: String!
+  }
+  type Mutation {
+    addVideo(video: VideoInput): Video
+    getVideo(id: ID): Video
+    deleteVideo(id: ID): Video
   }
 `;
 

695bf2df2d3f4ece03b56eec5fbd397ece9d8409
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2022-05-31 17:25:10 +0530

    added react-youtube for playing videos

--- client/simplay/src/pages/video/VideoPage/VideoPage.jsx
+++ client/simplay/src/pages/video/VideoPage/VideoPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useQuery } from "urql";
 import { getAllVidoesQuery } from "../videoPageQueries";
-
+import YouTube from "@u-wave/react-youtube";
 const VideoPage = () => {
   const [result, reexecuteQuery] = useQuery({
     query: getAllVidoesQuery,
@@ -10,7 +10,30 @@ const VideoPage = () => {
   if (fetching) return <h1>loading...</h1>;
   if (error) return <h1>Oh something went wrong! {error.message}</h1>;
   console.log({ data });
-  return <div>this is the video listing page</div>;
+  return (
+    <div
+      style={{
+        marginLeft: "auto",
+        marginRight: "auto",
+        border: "2px solid red",
+      }}
+    >
+      <h2>this is the video listing page</h2>
+      <div
+        style={{
+          margin: "1rem",
+          border: "2px solid yellow",
+        }}
+      >
+        <YouTube
+          width={330}
+          height={200}
+          video="5yTyKmNAxMA"
+          suggestedQuality={480}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default VideoPage;
